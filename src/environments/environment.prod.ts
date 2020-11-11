1e9f64d408fc7535ffcf15cb25cdbd25e247fb79
Cesar Garcia <cesar.garcia@citricosoft.com>
2020-11-10 21:45:06 -0600

    changes keys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   auth0: {
     clientID: "l4O2XdgEPbEj1MqMr3xXUVBj6cVdk3R2",
     domain: "dev-hu7u6gyk.us.auth0.com",
-    redirectUri: "http://localhost:4200",
-    logoutUrl: "http://localhost:4200"
+    redirectUri: "https://cesargarcia-dot.github.io/audio-player/",
+    logoutUrl: "https://cesargarcia-dot.github.io/audio-player/"
   }
 };

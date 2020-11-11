09680ca820cc15c3833468165ab563a5ad3dfbdc
Cesar Garcia <cesar.garcia@citricosoft.com>
2020-11-10 21:25:21 -0600

    change to evo prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  auth0: {
+    clientID: "l4O2XdgEPbEj1MqMr3xXUVBj6cVdk3R2",
+    domain: "dev-hu7u6gyk.us.auth0.com",
+    redirectUri: "http://localhost:4200",
+    logoutUrl: "http://localhost:4200"
+  }
 };

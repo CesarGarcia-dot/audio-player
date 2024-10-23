5aa149796738e0c294fa161892bf104948449bb8
Cesar_Garcia <juliocesar52009@live.com>
2024-10-23 16:39:16 -0600

    Update environment.prod.ts
    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   auth0: {
-    clientID: "l4O2XdgEPbEj1MqMr3xXUVBj6cVdk3R2",
-    domain: "dev-hu7u6gyk.us.auth0.com",
+    clientID: "",
+    domain: "",
     redirectUri: "https://cesargarcia-dot.github.io/audio-player/",
     logoutUrl: "https://cesargarcia-dot.github.io/audio-player/"
   }
